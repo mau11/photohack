ac261baff99b2ecf06aa25f645f6afc140fb2b13
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-07-06 12:22:06 -0400

    Initial setup

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Layout extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>The Photo Hack</h1>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  }
+}
