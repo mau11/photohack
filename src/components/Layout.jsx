893ded50541bde9698a9e9c8cf4059ff8a3a72be
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-07-07 02:25:40 -0400

    Add footer and pixabay attribution

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,12 +6,18 @@ export default class Layout extends Component {
 
   render() {
     return (
-      <div className="container">
-        <h1 className="font-weight-light text-center mt-4 mb-2">
+      <div className="container text-center">
+        <h1 className="font-weight-light mt-4 mb-2">
           Image Finder
         </h1>
-        <hr class="mt-2 mb-5"/>
+        <h6 className="font-weight-light text-center"> Search millions of royalty free images
+        </h6>
+        <hr className="mt-2 mb-5"/>
         <Search />
+        <footer className="footer">
+          <p className="container mb-10">Search results brought to you by <a href="https://pixabay.com/">Pixabay.</a>
+          </p>
+        </footer>
       </div>
     )
   }
